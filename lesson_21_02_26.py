b61eab5892cbfab0b89940080a176df6b96af2ec
svpustovoit <svpustvoit@gmail.com>
2026-02-21 17:36:36 +0200

    lesson_21_02_26 2 commit

--- lesson_21_02_26.py
+++ lesson_21_02_26.py
@@ -11,7 +11,7 @@ print('''
 однорядковий коментар
 однорядковий коментар
 ''')
-"""
+
 def first_function():
     print('Hello students!')
 
@@ -26,6 +26,25 @@ def second_function():
 print(second_function)
 print(second_function())
 #print(hello, name) ---> print("Hello students!", "Serhii")
+
+def hello(arg_1, arg_2):
+    return arg_1 + arg_2
+
+print(hello)
+print(hello("Hello", "World!"))
+print(hello(3, 5))
+print(hello(input("arg_1-"), input("arg_2-")))
+x = "IT"
+y = " Step"
+print(hello(x, y))
+"""
+def s_triangle(a, h):
+    s = .5 * a * h
+    return s
+
+print(f"площа трикутника s = {s_triangle(5, 6)}")
+print(f"площа трикутника s ="
+      f" {s_triangle(int(input('a=')), int(input('h=')))}")
 
 
 
